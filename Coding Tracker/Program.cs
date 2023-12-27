ddac44bb6b336034f8d7133e0acaaa39290e17bd
DLee211 <daniel.lee1921@gmail.com>
2023-12-27 11:25:06 -0500

    Initial commit

---
+++ Coding Tracker/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace Coding_Tracker
+{
+    class Program
+    {
+        private static string connectionString = @"Data Source = habit-tracker.db";
+
+        static void Main(string[] args)
+        {
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var tableCmd = connection.CreateCommand();
+                
+                tableCmd.CommandText = @"CREATE TABLE IF NOT EXISTS drinking_water(
+                                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        Date TEXT, 
+                                        Quantity INTEGER)";
+
+                tableCmd.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
+    }
+}
